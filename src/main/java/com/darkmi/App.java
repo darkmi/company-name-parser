6b53ee948dd378d2999349398baf6fc376cdbefa
darkmi <darkmi@126.com>
2024-07-11 17:31:01 +0800

    引入结巴分词。

--- src/main/java/com/darkmi/App.java
+++ src/main/java/com/darkmi/App.java
@@ -1,5 +1,10 @@
 package com.darkmi;
 
+import com.huaban.analysis.jieba.JiebaSegmenter;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("武汉海明智业电子商务有限公司");
+        JiebaSegmenter segmenter = new JiebaSegmenter();
+        List result = segmenter.sentenceProcess("武汉海明智业电子商务有限公司");
+        System.out.println(Arrays.toString(result.toArray()));
     }
 }
